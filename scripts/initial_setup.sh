788f645a8c75635a593e69b359bbc49f31967730
Thomas Batten <stenstorpmc@gmail.com>
2019-08-15 15:27:24 +0930

    initial-setup: make lib64 a link to lib

--- scripts/initial_setup.sh
+++ scripts/initial_setup.sh
@@ -3,17 +3,21 @@ source variables
 
 mkdir -pv ${LJOS}
 
-mkdir -pv ${LJOS}/{bin,boot/grub,dev,{etc/,}opt,home,lib/{firmware,modules},lib64,mnt}
+mkdir -pv ${LJOS}/{bin,boot/grub,dev,{etc/,}opt,home,lib/{firmware,modules},mnt}
 mkdir -pv ${LJOS}/{proc,media/{floppy,cdrom},sbin,srv,sys}
 mkdir -pv ${LJOS}/var/{lock,log,mail,run,spool}
 mkdir -pv ${LJOS}/var/{opt,cache,lib/{misc,locate},local}
 install -dv -m 0750 ${LJOS}/root
 install -dv -m 1777 ${LJOS}{/var,}/tmp
 install -dv ${LJOS}/etc/init.d
-mkdir -pv ${LJOS}/usr/{,local/}{bin,include,lib{,64},sbin,src}
+mkdir -pv ${LJOS}/usr/{,local/}{bin,include,lib,sbin,src}
 mkdir -pv ${LJOS}/usr/{,local/}share/{doc,info,locale,man}
 mkdir -pv ${LJOS}/usr/{,local/}share/{misc,terminfo,zoneinfo}
 mkdir -pv ${LJOS}/usr/{,local/}share/man/man{1,2,3,4,5,6,7,8}
+
+ln -sv lib ${LJOS}/lib64
+ln -sv lib ${LJOS}/usr/lib64
+
 for dir in ${LJOS}/usr{,/local}; do
   ln -sv share/{man,doc,info} ${dir}
 done
