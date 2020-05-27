91ebe2ee39e605248f68e42dde67f7dc8d713076
Thomas Batten <stenstorpmc@gmail.com>
2020-05-27 16:01:17 +0930

    download-all: download only required source

--- scripts/download-all.sh
+++ scripts/download-all.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 source config
 source variables/variables.common
+source variables/${LJOS_ARCH}/components
 
-for f in ${COMPONENTS_DIR}/*; do
+for f in ${CROSS_COMPILER} ${SYSTEM} ${CROSS_SYSTEM}; do
 	unset name version source folder build filename
 
-	source $f/info
+	source ${COMPONENTS_DIR}/$f/info
 	filename="${source##*/}"
 
 	if [ ! -e ${SOURCE_DIR}/${filename} ]; then
